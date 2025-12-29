12acc36f971e02793668998429ffb529f971c298
Anna Marochkina <annamarochkina2406@gmail.com>
2025-12-29 20:06:46 +0300

    fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,12 @@
 import { AppRouter } from "./providers/router"
 import { ToastProvider } from "./providers/toast"
 import { AppStateProvider } from "./providers/app-state"
-import { DebugConsole } from "@shared/ui"
 
 export function App() {
   return (
     <AppStateProvider>
       <ToastProvider>
         <AppRouter />
-        <DebugConsole />
       </ToastProvider>
     </AppStateProvider>
   )
